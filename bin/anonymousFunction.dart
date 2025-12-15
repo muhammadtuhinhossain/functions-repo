fa2638b8821b0521daab207a4facd2c2bfe7018a
MohammadTuhin1 <muhammadtuhin.dev@gmail.com>
2025-12-15 14:55:07 +0600

    Add anonymous function example

---
+++ bin/anonymousFunction.dart
@@ -0,0 +1,16 @@
+void main(){
+  anonymousFunctionExample();
+}
+void anonymousFunctionExample(){
+  print('--Anonymous Function Example--');
+
+  var greet= (String name){
+    print('Heelo, $name!');
+  };
+  greet('Tuhin');
+  var numbers=[1, 2,3];
+
+  numbers.forEach((num){
+    print('Number:$num');
+  });
+}
